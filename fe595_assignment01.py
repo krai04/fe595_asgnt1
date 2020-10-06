f6c9eb252327d2803b915834097f62b193b94779
Kshitij Rai <70661460+krai04@users.noreply.github.com>
2020-10-05 20:19:37 -0400

    Add files via upload

---
+++ fe595_assignment01.py
@@ -0,0 +1,14 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(0,2*np.pi,0.1)   # start,stop,step
+y = np.sin(x)
+z = np.cos(x)
+plt.plot(x,y,x,z)
+plt.show()
+
+plt.ylim(-3,3)
+
+plt.plot(x, np.tan(x))
+plt.show()
