4d4a2129d45da18ad2bb7ae6822aa18da6efaf39
Simran Jariwala <simranjariwala@simrans-mbp.myfiosgateway.com>
2020-10-05 21:33:33 -0400

    Adding updated file

--- fe595_refresher_final.py
+++ fe595_refresher_final.py
@@ -5,6 +5,10 @@ import numpy as np
 x = np.arange(0,2*np.pi,0.1)   # start,stop,step
 y = np.sin(x)
 z = np.cos(x)
+t = np.tan(x)
 plt.plot(x,y,x,z)
 plt.show()
-
+# to improve the scalability and plot tangent function
+plt.plot(x,t)
+plt.ylim(-3,3)
+plt.show()
